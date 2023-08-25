5c328ac6be858971c74dcc827921aeabe9af3c43
ryan-chaves-sao <ryan.chaves@cfa.harvard.edu>
2023-08-25 09:46:06 -0400

    Works with two different types of captures!

--- pcap2vdif.py
+++ pcap2vdif.py
@@ -27,17 +27,30 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     # Setup stuff for naming the output files
     vdif_outfile_stem = PurePath(args.pcapfile).stem
-    frame_no = 0
 
-    # Extract all UDP datagrams
-    extraction = extract(fin=args.pcapfile, nofile=True, protocol=UDP)
-    for frame in extraction.frame:
-        if UDP in frame:
-            # TODO Should we check anything else?
-            vdif_frame = frame[UDP].payload.data
-            outfile = f'{vdif_outfile_stem}_{frame_no}.vdif'
-            with open(outfile, 'wb') as of:
-                of.write(vdif_frame)
+    # Extract all frames and attempt IPv4 reassembly
+    extraction = extract(fin=args.pcapfile, nofile=True, store=True,
+                         reassembly=True, reasm_strict=True, ipv4=True)
+
+    # Inspect the reassembled IPv4 packets if we got them
+    if len(extraction.reassembly.ipv4) > 0:
+        for dgram_no, dgram in enumerate(extraction.reassembly.ipv4):
+            if UDP in dgram.packet:
+                # TODO Should we check anything else?
+                vdif_frame = dgram.packet.payload.data
+                outfile = f'{vdif_outfile_stem}_{dgram_no}.vdif'
+                with open(outfile, 'wb') as of:
+                    of.write(vdif_frame)
+    else:
+        # pcapkit didn't reassemble IPv4 from the capture, so ... ¯\_(ツ)_/¯
+        for frame_no, frame in enumerate(extraction.frame):
+            if frame.protocol == 'NULL':
+                # The frame is from a loopback interface and so we assume it's a full UDP datagram.
+                # We assume a 4-byte link layer header, 20-byte IP header, and 8-byte UDP header.
+                vdif_frame = frame.payload.data[4+20+8:]
+                outfile = f'{vdif_outfile_stem}_{frame_no}.vdif'
+                with open(outfile, 'wb') as of:
+                    of.write(vdif_frame)
 
     return 0
 
