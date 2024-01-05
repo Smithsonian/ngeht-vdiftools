eb540228cd6bf6e9747421e211b5012fc117ea54
ryan-chaves-sao <ryan.chaves@cfa.harvard.edu>
2024-01-05 09:51:53 -0500

    Output by src/dst pair rather than just by src. Refactor some common processing. Update -h documentation.

--- pcap2vdif.py
+++ pcap2vdif.py
@@ -18,11 +18,22 @@ import ipaddress
 # pcapkit imports
 from pcapkit import extract, UDP, IPv4
 
+# Dict for storing frames per source and dest IP. Needs to be referenced via 'global' keyword.
+# TODO The pythonic way to deal with this would probably be to make a class.
+collected_frames = {}
+
 
 def main(argv: Sequence[str] | None = None) -> int:
     # Process commandline arguments
     parser = argparse.ArgumentParser(
-        description="""Extracts UDP datagram payloads from a PCAP capture file.""",
+        description="""
+        Extracts UDP datagram payloads from a PCAP capture file.
+
+        Each VDIF frame will be stored in a file named
+        {pcapfilestem}_{src_ip}_{dst_ip}_{packet#}.vdif, unless the --single-vdif argument is used.
+        In that case, all VDIF frames identified for a src_ip/dst_ip pair will be stored in a single
+        file using the same naming convention but without the {packet#} component.
+        """,
         epilog=f"""This script is called {PurePath(__file__).stem} because it's assumed this will
             be used on PCAP captures that contain VDIF data frames inside UDP datagrams.""",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -56,21 +67,16 @@ def main(argv: Sequence[str] | None = None) -> int:
             if frame_no == end_packet:
                 break
 
-    # For single-vdif mode. We use a dict so we can capture frames per source IP.
-    collected_frames = {}
-
     # Inspect the reassembled IPv4 packets if we got them
     if (not args.r) and (len(extraction.reassembly.ipv4) > 0):
         for dgram_no, dgram in enumerate(extraction.reassembly.ipv4):
             if UDP in dgram.packet:
                 src = dgram.id.src
-                if args.single_vdif:
-                    if src not in collected_frames:
-                        collected_frames[src] = bytearray()
-                    collected_frames[src] += dgram.packet.payload.data
-                else:
-                    write_vdif_file(
-                        dgram.packet.payload.data, f'{vdif_outfile_stem}_{src}', dgram_no)
+                dst = dgram.id.dst
+                process_packet(
+                    dgram.id.src, dgram.id.dst, dgram.packet.payload.data, vdif_outfile_stem,
+                    not args.single_vdif, dgram_no
+                )
     else:
         # pcapkit didn't reassemble IPv4 from the capture, so ... ¯\_(ツ)_/¯
         for frame_no, frame in enumerate(extraction.frame):
@@ -80,36 +86,62 @@ def main(argv: Sequence[str] | None = None) -> int:
                     # datagram. We assume a 4-byte link layer header, 20-byte IP header, and 8-byte
                     # UDP header.
                     src = ipaddress.ip_address(frame.payload.data[4+12:4+16])
-                    if args.single_vdif:
-                        if src not in collected_frames:
-                            collected_frames[src] = bytearray()
-                        collected_frames[src] += frame.payload.data[4+20+8:]
-                    else:
-                        write_vdif_file(
-                            frame.payload.data[4+20+8:], f'{vdif_outfile_stem}_{src}', frame_no)
+                    dst = ipaddress.ip_address(frame.payload.data[4+16:4+20])
+                    process_packet(
+                        src, dst, frame.payload.data[4+20+8:], vdif_outfile_stem,
+                        not args.single_vdif, frame_no
+                    )
                 elif (frame.protocol == 'Ethernet') and (UDP in frame):
                     # This is an Ethernet frame that pcapkit could decode and no reassembly was
                     # required. We can look for UDP directly.
                     src = frame[IPv4].src
-                    if args.single_vdif:
-                        if src not in collected_frames:
-                            collected_frames[src] = bytearray()
-                        collected_frames[src] += frame[UDP].payload.data
-                    else:
-                        write_vdif_file(
-                            frame[UDP].payload.data, f'{vdif_outfile_stem}_{src}', frame_no)
+                    dst = frame[IPv4].dst
+                    process_packet(
+                        src, dst, frame[UDP].payload.data, vdif_outfile_stem, not args.single_vdif,
+                        frame_no
+                    )
                 else:
                     pass
 
     if args.single_vdif:
+        # Output all frames for each src/dst pair in a single VDIF file.
+        global collected_frames
+
         for src in collected_frames:
-            if len(collected_frames[src]) > 0:
-                write_vdif_file(collected_frames[src], f'{vdif_outfile_stem}_{src}')
+            for dst in collected_frames[src]:
+                if len(collected_frames[src][dst]) > 0:
+                    write_vdif_file(collected_frames[src][dst], f'{vdif_outfile_stem}_{src}_{dst}')
 
     return 0
 
 
+def process_packet(
+    src: ipaddress.IPv4Address,
+    dst: ipaddress.IPv4Address,
+    vdif_frame: bytearray,
+    vdif_outfile_stem: str,
+    file_per_frame: bool = False,
+    packet_no=None
+) -> None:
+    """Utility function for common handling of packet data."""
+    global collected_frames
+
+    if src not in collected_frames:
+        # Add a new entry if we haven't seen this src yet.
+        collected_frames[src] = {}
+        collected_frames[src][dst] = bytearray()
+
+    # Append the VDIF frame for the src/dst pair.
+    collected_frames[src][dst] += vdif_frame
+
+    if file_per_frame:
+        # Write out the file for this VDIF frame
+        write_vdif_file(
+            collected_frames[src][dst], f'{vdif_outfile_stem}_{src}_{dst}', packet_no)
+
+
 def write_vdif_file(vdif_frames, file_stem, frame_no=None):
+    """Utility function for writing a VDIF file."""
     outfile = f'{file_stem}'
     if None is not frame_no:
         outfile += f'_{frame_no}'
